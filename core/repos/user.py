3a42f3552460765e8d05a09aef2cb9575dc8fa1c
unknown <ro9aine@gmail.com>
2025-10-02 14:03:56 +0300

    feat: add login method for auth api

--- core/repos/user.py
+++ core/repos/user.py
@@ -4,6 +4,10 @@ from core.entities.user import User
 
 class AbstractUserRepo(ABC):
     @abstractmethod
+    def find_by_username(self, username: str) -> User | None:
+        pass
+
+    @abstractmethod
     def save(self, user: User.Creation) -> User:
         pass
 
@@ -11,6 +15,12 @@ class AbstractUserRepo(ABC):
 class InMemoryUserRepo(AbstractUserRepo):
     _storage: list[User] = []
     _last_id: int = 0
+
+    def find_by_username(self, username: str) -> User | None:
+        for user in self._storage:
+            if user.username == username:
+                return user
+        return None
 
     def save(self, user: User.Creation) -> User:
         self._last_id += 1
--- core/services/auth.py
+++ core/services/auth.py
@@ -11,6 +11,14 @@ class AuthService:
         self.user_repo = user_repo
         self.participant_repo = participant_repo
 
+    def login(self, username: str, password: str) -> User:
+        user = self.user_repo.find_by_username(username)
+        if user is None:
+            raise ValueError(f"User {username} not found")
+        if user.hashed_password != hash_password(password):
+            raise ValueError("Incorrect password")
+        return user
+
     def register(self, username: str, pure_password: str) -> tuple[User, Chat]:
         user = self.user_repo.save(User.Creation(
             username=username,
