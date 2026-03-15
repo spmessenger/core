11225a2312e1cc4b028320d635094c37e5ff0958
CodexSandboxOffline <ro9aine@gmail.com>
2026-03-16 01:08:38 +0300

    Add message listing support

--- core/repos/message.py
+++ core/repos/message.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from sqlalchemy import asc, select
 from core.entities.message import Message
 from db.models import Message as ModelMessage
 from db.session import session_factory, Session
@@ -6,6 +7,10 @@ from .base import InMemoryRepo, DbRepo
 
 
 class AbstractMessageRepo(ABC):
+    @abstractmethod
+    def find_all(self, chat_id: int | None = None) -> list[Message]:
+        pass
+
     @abstractmethod
     def save(self, message: Message.Creation) -> Message:
         pass
@@ -16,11 +21,26 @@ class DbMessageRepo(DbRepo, AbstractMessageRepo):
     entity_model = Message
 
     @session_factory
+    def find_all(self, chat_id: int | None = None, *, session: Session) -> list[Message]:
+        query = select(self.model).order_by(asc(self.model.created_at_timestamp))
+
+        if chat_id is not None:
+            query = query.where(self.model.chat_id == chat_id)
+
+        messages = session.execute(query).scalars().all()
+        return [Message.model_validate(message, from_attributes=True) for message in messages]
+
+    @session_factory
     def save(self, message: Message.Creation, *, session: Session) -> Message:
         return super().save(message, session=session)
 
 
 class InMemoryMessageRepo(AbstractMessageRepo, InMemoryRepo[Message]):
+    def find_all(self, chat_id: int | None = None) -> list[Message]:
+        if chat_id is None:
+            return list(self._storage)
+        return [message for message in self._storage if message.chat_id == chat_id]
+
     def save(self, message: Message.Creation) -> Message:
         entity = Message(
             id=0,
